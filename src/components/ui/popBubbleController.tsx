0c6ad1fc3cfac26cd7bd1252a49ff7075370dfb5
desilvakdn <desilvakdn2@gmail.com>
2024-08-23 13:10:07 +0530

    feat(home-page): added a animation for home page and replaced images

---
+++ src/components/ui/popBubbleController.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import PopBubble from "./popBubble";
+
+interface BubbleType {
+  id: number;
+  key: number;
+}
+
+const PopBubbleController = () => {
+  const [bubbles, setBubbles] = useState<BubbleType[]>([]);
+
+  useEffect(() => {
+    const createBubble = () => {
+      setBubbles((prev) => [...prev, { id: Math.random(), key: Date.now() }]);
+    };
+
+    const intervalId = setInterval(() => {
+      if (bubbles.length < 100) {
+        createBubble();
+      }
+    }, 10);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <>
+      {bubbles.map((bubble, index) => (
+        <PopBubble key={bubble.key} />
+      ))}
+    </>
+  );
+};
+
+export default PopBubbleController;
